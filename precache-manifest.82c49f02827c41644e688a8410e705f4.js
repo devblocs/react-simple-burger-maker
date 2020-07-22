23ceb289f5d1f575be6fa098d7f946c8562eb513
venkatesh nadar <venkatesh.nadar@neosofttech.com>
2020-07-22 18:46:41 +0530

    Updates

---
+++ precache-manifest.82c49f02827c41644e688a8410e705f4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebeea5893eb00393732985882e52808d",
+    "url": "/react-simple-burger-maker/index.html"
+  },
+  {
+    "revision": "51b4fd7739c45be0e555",
+    "url": "/react-simple-burger-maker/static/css/main.812876ec.chunk.css"
+  },
+  {
+    "revision": "38b298f12285fe2e1628",
+    "url": "/react-simple-burger-maker/static/js/2.66427933.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-simple-burger-maker/static/js/2.66427933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51b4fd7739c45be0e555",
+    "url": "/react-simple-burger-maker/static/js/main.be548146.chunk.js"
+  },
+  {
+    "revision": "85db0f93e560c4f9cf11",
+    "url": "/react-simple-burger-maker/static/js/runtime-main.ff474625.js"
+  },
+  {
+    "revision": "9137c4f40fbe5d086100dbf23b67ffd2",
+    "url": "/react-simple-burger-maker/static/media/bottom.9137c4f4.jpg"
+  },
+  {
+    "revision": "d0e61dc2cb2d041159c7d77d381c7e57",
+    "url": "/react-simple-burger-maker/static/media/meat.d0e61dc2.jpg"
+  },
+  {
+    "revision": "63f7e0bcfe69cd70c3dc19e81698841a",
+    "url": "/react-simple-burger-maker/static/media/tomato.63f7e0bc.jpg"
+  },
+  {
+    "revision": "719e7ddb9d39363e4614d4568988fab4",
+    "url": "/react-simple-burger-maker/static/media/top.719e7ddb.jpg"
+  }
+]);
